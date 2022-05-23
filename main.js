1e1c5272116811e975d6f0d0ee05a160f80bccc3
Gail Giles <gailgiles@Gails-MacBook-Pro.local>
2022-05-23 12:41:41 -0400

    adding js

---
+++ main.js
@@ -0,0 +1,20 @@
+const searchForm = document.querySelector("form");
+const searchResultDiv = document.querySelector(".search-result");
+const container = document.querySelector(".container");
+let searchQuery = "";
+const APP_ID = 'dba31816';
+const APP_key = '0e72894b563bde0deb7baa4ed94d01cd';
+// console.log(container)
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchQuery = e.target.querySelector("input").value;
+  fetchAPI();
+});
+
+async function fetchAPI() {
+  const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_key}&from=0&to=20`;
+  const response = await fetch(baseURL);
+  const data = await response.json();
+  generateHTML(data.hits);
+  console.log(data);
+}
